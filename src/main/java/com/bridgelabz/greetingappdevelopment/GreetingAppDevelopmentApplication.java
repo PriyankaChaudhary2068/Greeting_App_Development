35c9dc9fbb752e0474f641901fa83db648aad05e
chutki12 <priyankachaudhary2068>
2022-08-28 16:08:45 +0530

    ADDED : Welcome To Greeting App Development!

---
+++ src/main/java/com/bridgelabz/greetingappdevelopment/GreetingAppDevelopmentApplication.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.greetingappdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppDevelopmentApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome To Greeting App Development!");
+        SpringApplication.run(GreetingAppDevelopmentApplication.class, args);
+    }
+
+}
